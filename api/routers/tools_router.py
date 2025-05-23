b0c596b4b7edf28ed2b77fea1bd37af2c438d19a
mohan.kumar2 <mohan.kumar2@apexon.com>
2025-05-23 09:51:06 +0530

    tools, api

--- api/routers/tools_router.py
+++ api/routers/tools_router.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, HTTPException, Depends
+from pydantic import BaseModel
 from typing import List
 import logging
 
@@ -9,6 +10,7 @@ router = APIRouter(
     prefix="/tools",
     tags=["Tools"]
 )
+
 logger = logging.getLogger(__name__)
 
 @router.get("/", response_model=List[ToolDefinition])
@@ -18,3 +20,27 @@ async def api_list_available_tools():
     except Exception as e:
         logger.error(f"Error listing tools: {e}", exc_info=True)
         raise HTTPException(status_code=500, detail="Failed to list tools.")
+
+class ToolRequest(BaseModel):
+    id: str
+
+@router.post("/tool-name")
+# async def record_audio(request: RecordRequest):
+async def tool_instance(request:ToolRequest):
+    try:
+        #res = tool_service.list_tool_instance(request.id)
+        tool_config = tool_service.list_tool_instance(request.id)
+        if not tool_config:
+            raise HTTPException(status_code=404, detail=f"No tool found with id '{request.id}'")
+
+        tool_class = tool_config["class"]
+
+        return {
+            "id": request.id,
+            "name": tool_config["name"],
+            "description": tool_config["description"],
+            "class_name": tool_class.__name__,
+            "parameters_schema": tool_config["parameters_schema"]
+        }
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
--- services/tool_service.py
+++ services/tool_service.py
@@ -1,6 +1,10 @@
 from typing import List
 from schemas.tools_schema import ToolDefinition
-from tool_registry.registry import get_all_tool_definitions
+from tool_registry.registry import get_all_tool_definitions, get_tool_instance, get_tool_instance_id
+
 
 def list_available_tools() -> List[ToolDefinition]:
     return get_all_tool_definitions()
+
+def list_tool_instance(id):
+    return get_tool_instance_id(id)
