b3669d0b004900adf3f5767a1b7ddd53d215d835
mohan.kumar2 <mohan.kumar2@apexon.com>
2025-06-13 19:14:03 +0530

    upload functionality

--- db/models.py
+++ db/models.py
@@ -1,7 +1,7 @@
+import uuid
+from sqlalchemy.sql import func
 from sqlalchemy import Column, String, DateTime, JSON, Boolean
 from sqlalchemy.dialects.postgresql import UUID as DB_UUID
-from sqlalchemy.sql import func
-import uuid
 
 from .database import Base
 
@@ -14,3 +14,12 @@ class DBWorkflow(Base):
     is_deleted = Column(Boolean, default=False, nullable=False, index=True)
     created_at = Column(DateTime(timezone=True), server_default=func.now())
     updated_at = Column(DateTime(timezone=True), server_default=func.now(), onupdate=func.now())
+
+
+class Files(Base):
+    __tablename__ = "files"
+
+    id = Column(DB_UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    file_name = Column(String, index=True, nullable=False)
+    file_url = Column(String, index=True, nullable=False)
+    date_time = Column(DateTime(timezone=True), server_default=func.now())
